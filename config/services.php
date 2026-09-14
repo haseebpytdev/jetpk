2ac730c5eac5dc24938448fc45c75a5ac9e3bbeb
haseebpytdev <myworkhaseeb@gmail.com>
2026-09-14 12:31:20 +0500

    feat(seo): add Search Console verification hook

--- config/services.php
+++ config/services.php
@@ -39,6 +39,7 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI') ?: rtrim((string) env('APP_URL', ''), '/').'/auth/google/callback',
+        'site_verification' => env('GOOGLE_SITE_VERIFICATION'),
     ],
 
     'facebook' => [
